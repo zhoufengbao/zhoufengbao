e2cb98baca866a3a79498dfcfdafc45de6a37af5
Jobs <Jobs@apple.com>
2017-07-23 17:31:30 +0800

    t

---
+++ js/t.js
@@ -0,0 +1,20 @@
+
+var callback = function(){
+  // Handler when the DOM is fully loaded
+var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+var firstDate = new Date('3/6/2017');
+var secondDate = new Date();
+
+var diffDays = Math.round(Math.abs((secondDate.getTime() - firstDate.getTime())/(oneDay)));
+
+document.getElementById('days').innerHTML = diffDays
+};
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+  callback();
+} else {
+  document.addEventListener("DOMContentLoaded", callback);
+}
